47b91cc5c584e620da76fec2c5da9dc4f756883f
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-02-04 10:17:00 -0300

    feat: created login util

---
+++ src/util/PostLogin.tsx
@@ -0,0 +1,23 @@
+import axios, { AxiosResponse } from "axios"
+
+export interface User {
+    email: string;
+    password: string;
+}
+
+const API = process.env.PUBLIC_NEXT_API_HOMOLOG_USER;
+
+export const Login = async (credentials: User): Promise<AxiosResponse<any>> => {
+    try {
+        const response: AxiosResponse<any> = await axios.post(`${API}/auth/login`, credentials, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response;
+    } catch (error) {
+        console.log("Error ao estabelecer conexão: ", error);
+        throw error;
+    }
+};
+
